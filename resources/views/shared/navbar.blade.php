e430645be620ddc904b3d726064816a155f7cdc6
mr.kamy <steam-punker@mail.ru>
2018-06-18 16:02:44 +0300

    start views

---
+++ resources/views/shared/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-light">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <div class="navbar-brand">Learning Laravel</div>
+        </div>
+        <div class="navbar navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="/">Home</a> </li>
+            </ul>
+        </div>
+    </div>
+</nav>
